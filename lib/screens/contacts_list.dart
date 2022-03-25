4acf8aaed739c5c02ab11d02682ecf4fedd0e4ad
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-25 19:08:04 -0300

    refactored to render new real-time contact

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -3,34 +3,68 @@ import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
-class ContactsList extends StatelessWidget {
+class ContactsList extends StatefulWidget {
   const ContactsList({Key? key}) : super(key: key);
 
   @override
+  State<ContactsList> createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
+
+  @override
   Widget build(BuildContext context) {
+    List<Contact> contacts;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Contacts"),
       ),
-      body: FutureBuilder(
-        //Nesta propriedade fica a nossa função assíncrona
-        future: findAll(),
-        //Assim que tiver uma resposta da pripriedade acima, ele irá modificar o código aqui dentro deste builder abaixo:
-        builder: (context, snapshot) {
-          final List<Contact> contacts = snapshot.data as List<Contact>;
-          return ListView.builder(
-            itemBuilder: (context, index) =>
-                _ContactItem(contact: contacts[index]),
-            itemCount: contacts.length,
-          );
-        },
-      ),
+      body: FutureBuilder<List<Contact>>(
+          //Este InitialData abaixo será renderizado (uma lista vazia) desde o início, antes da future ser resolvida
+          // initialData: const [],
+          //Nesta propriedade fica a nossa função assíncrona
+          future: findAll(),
+          //Assim que tiver uma resposta da pripriedade acima, ele irá modificar o código aqui dentro deste builder abaixo:
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                break;
+
+              case ConnectionState.waiting:
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      CircularProgressIndicator(),
+                      Text("Loading")
+                    ],
+                  ),
+                );
+
+              case ConnectionState.active:
+                //Retorna partes carregadas do conteúdo -> Stream
+                break;
+
+              case ConnectionState.done:
+                contacts = snapshot.data as List<Contact>;
+                return ListView.builder(
+                  itemBuilder: (context, index) =>
+                      _ContactItem(contact: contacts[index]),
+                  itemCount: contacts.length,
+                );
+            }
+            return const Text("Unknown Error");
+          }),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).colorScheme.primary,
         onPressed: () {
-          Navigator.of(context)
-              .push(
-                  MaterialPageRoute(builder: (context) => const ContactForm()))
-              .then((newContact) => print(newContact));
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const ContactForm()))
+          .then((newContact) => newContact != null ? setState(() => { } ) : null );
         },
         child: const Icon(Icons.add),
       ),
