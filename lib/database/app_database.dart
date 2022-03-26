24feee9a0facb91ff0fd0d502be3be996c05af89
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-25 22:09:39 -0300

    refactored the database

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,52 +2,45 @@ import 'package:bytebank/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-//Função responsável por criar o banco de dados
-Future<Database> createDataBase() {
-  //Função do sqflite que permite obter o local de banco de dados padrão
-  //Como ele retorna um Future<String>, podemos utilizar o then para acessá-lo
-  return getDatabasesPath().then((dataBasePath) {
-    //Pegando o caminho junto com o nome de arquivo que defini
-    final String path = join(dataBasePath, "bytebank_database.db");
+//Função responsável por pegar o banco de dados
+//NOTA: A função que cria o db é passada como um parâmetro da openDatabase
+Future<Database> getDataBase() async {
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, "bytebank_database.db");
 
-    //Abra o banco de dados em um determinado caminho
-    //podendo ainda especificar a versão do db que está sendo aberta -> isto é usado para realizar determinadas ações como onCreate
-    return openDatabase(path, onCreate: (db, version) {
-  //aqui dentro podemos criar nossa tabela
-  //Execute uma consulta SQL sem valor de retorno. Dentro dos parenteses vão comandos SQL compatíveis com SQLite
-      db.execute('CREATE TABLE contacts('
-          'id INTEGER PRIMARY KEY AUTOINCREMENT, '
-          'name TEXT, '
-          'account_number INTEGER)');
-    }, version: 1);
-  });
+  return openDatabase(path, onCreate: _onCreate, version: 1);
 }
 
-//Função para salvar o contato
-Future<int> save(Contact contact) {
-  return createDataBase().then((db) {
-    //Precisamos aqui formatar o valor a ser salvo do jeito que a função insert pede:
-    final Map<String, dynamic> contactMap = {};
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNumber;
+void _onCreate(Database db, int version) async {
+  db.execute('CREATE TABLE contacts('
+      'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+      'name TEXT, '
+      'account_number INTEGER)');
+}
 
-    //Aqui, a partir de db, podemos executar ações como inserir (salvar) na tabela, passando o nome dela e o valor a ser salvo:
-    //O ID não tá sendo passado intencionalmente, pois o SQLite irá gerá-lo automaticamente
-    return db.insert("contacts", contactMap);
-  });
+//Função para salvar cada contato no banco de dados
+Future<int> save(Contact contact) async {
+  final Database db = await getDataBase();
+
+  //formatanto o valor a ser salvo do jeito que a insert pede
+  final Map<String, dynamic> contactMap = {};
+
+  contactMap["name"] = contact.name;
+  contactMap["account_number"] = contact.accountNumber;
+
+  return db.insert("contacts", contactMap);
 }
 
 //Definindo uma função para buscar todos os contatos
-Future<List<Contact>> findAll() {
-  return createDataBase().then((db) {
-    return db.query("contacts").then((maps) {
-      final List<Contact> contacts = [];
+Future<List<Contact>> findAll() async {
+  final Database db = await getDataBase();
 
-      for (Map<String, dynamic> map in maps) {
-          final Contact contact = Contact(map["id"], map["name"], map["account_number"]);
-          contacts.add(contact);
-      }
-      return contacts;
-    });
-  });
+  List<Map<String, dynamic>> dbMapsList = await db.query("contacts");
+
+  final List<Contact> contacts = [];
+
+  for (Map<String, dynamic> row in dbMapsList) {
+    contacts.add(Contact(row["id"], row["name"], row["account_number"]));
+  }
+  return contacts;
 }
