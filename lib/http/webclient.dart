1f698ec0d0cb7ec4ba315efb1638ec2da9853e2f
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-04-01 13:11:34 -0300

    creates the save method

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -6,6 +6,14 @@ import 'package:http_interceptor/http_interceptor.dart';
 
 import '../models/transaction.dart';
 
+//Instanciando o client interceptado
+Client client = InterceptedClient.build(interceptors: [
+  LoggingInterceptor(),
+]);
+
+const String urlAuthority = 'crudcrud.com';
+const String urlPath = 'api/bbe0815b844f48fda1b1e6f628c235d9/transactions';
+
 //O interceptador deve implementar a interface InterceptorContract
 class LoggingInterceptor implements InterceptorContract {
   //Todo tipo de requyisição vai chamar este método
@@ -29,15 +37,9 @@ class LoggingInterceptor implements InterceptorContract {
 }
 
 Future<List<Transaction?>?> findAll() async {
-  //Instanciando o client interceptado
-  Client client = InterceptedClient.build(interceptors: [
-    LoggingInterceptor(),
-  ]);
-
   late final Response response;
   try {
-    response = await client.get(Uri.https(
-        'crudcrud.com', 'api/bbe0815b844f48fda1b1e6f628c235d9/transactions'));
+    response = await client.get(Uri.https(urlAuthority, urlPath));
   } catch (e) {
     print('Erro capturado $e');
   }
@@ -48,17 +50,48 @@ Future<List<Transaction?>?> findAll() async {
     final List<dynamic> decodedJson = jsonDecode(response.body);
 
     for (Map<String, dynamic> transactionJson in decodedJson) {
-    final Transaction transaction = Transaction(
-      value: double.parse(transactionJson['value']),
-      contact: Contact(0, transactionJson['contact']['name'],
-          int.parse(transactionJson['contact']['accountNumber'])
-        )
-      );
-    transactions.add(transaction);
-  }
-  return transactions;
+      final Transaction transaction = Transaction(
+          value: transactionJson['value'] ,
+          contact: Contact(
+            0, 
+            transactionJson['contact']['name'], transactionJson['contact']['accountNumber']
+          )
+          );
+      transactions.add(transaction);
+    }
+    return transactions;
   } else {
     // throw Exception('Não foi possível carregar as transações');
     return null;
   }
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  //TODO: atenção utilizar dados que não são String dentro do jsonEncode
+  final String transactionJson = jsonEncode(
+  {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber
+    }
+  });
+
+  final Response response = await client.post(
+    Uri.https(urlAuthority, urlPath),
+    headers: {'Content-Type': 'application/json'},
+    body: transactionJson,
+  );
+  
+  final Map<String, dynamic> json = jsonDecode(response.body);
+
+  return Transaction(
+      value: json['value'],
+      contact: Contact(
+        0, 
+        json['contact']['name'], 
+        json['contact']['accountNumber']
+      )
+    );
+
 }
